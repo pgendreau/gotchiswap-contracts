2c9618374ff56111920c19c9938597d85cb88145
Philippe Gendreau <pgendreau@gmail.com>
2023-07-24 19:47:01 -0400

    add natspec comments

--- contracts/Gotchiswap.sol
+++ contracts/Gotchiswap.sol
@@ -5,6 +5,10 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import { SafeERC20, IERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import { IERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
+/**
+ * @title Gotchiswap
+ * @dev A decentralized escrow contract for trading Aavegotchi assets OTC style.
+ */
 contract Gotchiswap is Initializable {
 
     address public ghstAddress;
@@ -15,6 +19,9 @@ contract Gotchiswap is Initializable {
     IERC721 aavegotchi;
     IERC20 GHST;
 
+    /**
+     * @dev Struct representing a Gotchi/Portal trade.
+     */
     struct GotchiSale {
         uint256 id;
         uint256 gotchi;
@@ -22,6 +29,9 @@ contract Gotchiswap is Initializable {
         address buyer;
     }
 
+    /**
+     * @dev Struct representing a reference to a sale made by a seller for a buyer.
+     */
     struct SaleRef {
         address seller;
         uint256 id;
@@ -37,8 +47,11 @@ contract Gotchiswap is Initializable {
     event concludeSale(address indexed buyer, uint256 indexed gotchi);
     event abortSale(address indexed seller, uint256 indexed gotchi);
 
+    /**
+     * @dev Modifier that only allows the admin to perform certain functions.
+     */
     modifier onlyAdmin {
-        require(msg.sender == adminAddress);
+        require(msg.sender == adminAddress, "Gotchiswap: Only the admin can perform this action");
         _;
     }
 
@@ -47,6 +60,13 @@ contract Gotchiswap is Initializable {
         _disableInitializers();
     }
 
+    /**
+     * @dev Initializes the contract with the necessary addresses.
+     * @param _ghst Address of the GHST ERC20 token.
+     * @param _gotchis Address of the Aavegotchi ERC721 token.
+     * @param _wearables Address of the wearables contract (not used in this version).
+     * @param _admin Address of the admin who can perform certain actions.
+     */
     function initialize(
         address _ghst,
         address _gotchis,
@@ -62,89 +82,149 @@ contract Gotchiswap is Initializable {
         GHST = IERC20(ghstAddress);
     }
 
+    /**
+     * @dev Allows the admin to transfer admin rights to another address.
+     * @param _admin The new admin address.
+     */
     function changeAdmin(address _admin) external onlyAdmin {
-        require(_admin != address(0), "Cannot change admin: Invalid Address");
+        require(_admin != address(0), "Gotchiswap: Cannot change admin to an invalid address");
         adminAddress = _admin;
     }
 
+    /**
+     * @dev Allows the admin to remove the admin privilege completely.
+     *      This makes it impossible to use admin functions anymore. Should be used
+     *      in tandem with the renounceOwnership of the ProxyAdmin contract to make it
+     *      completely trustless and immutable
+     */
     function removeAdmin() external onlyAdmin {
         adminAddress = address(0);
     }
 
+    /**
+     * @dev Allows the admin to withdraw an Aavegotchi ERC721 token from the contract.
+     * @param _tokenId The ID of the ERC721 token to be withdrawn.
+     */
     function withdrawERC721(uint256 _tokenId) external onlyAdmin {
         aavegotchi.safeTransferFrom(address(this), adminAddress, _tokenId, "");
     }
 
+    /**
+     * @dev Allows the admin to withdraw GHST ERC20 tokens from the contract.
+     */
     function withdrawGHST() external onlyAdmin {
-        SafeERC20.safeTransferFrom(
+        SafeERC20.safeTransfer(
             GHST,
-            address(this),
             adminAddress,
             GHST.balanceOf(address(this))
         );
     }
 
+    /**
+     * @dev Gets the offer made for a buyer at a specific index.
+     * @param _buyer The address of the buyer.
+     * @param _index The index of the offer.
+     * @return seller The address of the seller who made the offer.
+     * @return id The ID of the offer.
+     */
     function getOffer(address _buyer, uint256 _index) external view returns (
         address,
         uint256
     ) {
-        require(isBuyer(_buyer), "Cannot get offer: No offers found");
+        require(isBuyer(_buyer), "Gotchiswap: No offers found for the buyer");
 
         SaleRef memory offer = buyers[_buyer][_index];
 
         return(offer.seller, offer.id);
     }
 
+    /**
+     * @dev Gets the sale made by a seller at a specific index.
+     * @param _seller The address of the seller.
+     * @param _index The index of the sale.
+     * @return id The ID of the sale.
+     * @return gotchi The ID of the Aavegotchi token being sold.
+     * @return price The price of the Aavegotchi token in GHST tokens.
+     * @return buyer The address of the buyer who can purchase the Aavegotchi token.
+     */
     function getSale(address _seller, uint256 _index) external view returns (
         uint256,
         uint256,
         uint256,
         address
     ) {
-        require(isSeller(_seller), "Cannot get sale: No sales found");
+        require(isSeller(_seller), "Gotchiswap: No sales found for the seller");
 
         GotchiSale memory sale = sellers[_seller][_index];
 
         return(sale.id, sale.gotchi, sale.price, sale.buyer);
     }
 
+    /**
+     * @dev Gets the number of offers made to a specific buyer.
+     * @param _buyer The address of the buyer.
+     * @return The number of sales made by the buyer.
+     */
     function getBuyerSalesCount(address _buyer) external view returns (uint256) {
-        require(isBuyer(_buyer), "No sales found");
+        require(isBuyer(_buyer), "Gotchiswap: No sales found for the buyer");
         return buyers[_buyer].length;
     }
 
+    /**
+     * @dev Gets the number of offers made by a specific seller.
+     * @param _seller The address of the seller.
+     * @return The number of sales made by the seller.
+     */
     function getSellerSalesCount(address _seller) external view returns (uint256) {
-        require(isSeller(_seller), "No sales found");
+        require(isSeller(_seller), "Gotchiswap: No sales found for the seller");
         return sellers[_seller].length;
     }
 
+    /**
+     * @dev Allows a seller to create a trade for their Aavegotchi with a buyer.
+     * @param _gotchi The ID of the Aavegotchi token to be sold.
+     * @param _price The price of the Aavegotchi token in GHST tokens.
+     * @param _buyer The address of the buyer who can purchase the Aavegotchi token.
+     */
     function sellGotchi(
         uint256 _gotchi,
         uint256 _price,
         address _buyer
-    ) external
-    {
+    ) external {
         require(
             _price > 0,
-            "Cannot add sale: Price must be greater than 0"
+            "Gotchiswap: Price must be greater than 0"
         );
-        // transfer gotchi to contract
+        // Transfer the Aavegotchi token to the contract
         aavegotchi.safeTransferFrom(msg.sender, address(this), _gotchi, "");
+        // Add the sale to the seller's sales list and the buyer's offers list
         addSale(msg.sender, _gotchi, _price, _buyer);
         emit newSale(msg.sender, _gotchi);
     }
 
+    /**
+     * @dev Allows a seller to abort their Aavegotchi sale.
+     * @param _index The index of the sale to be aborted.
+     */
     function abortGotchiSale(uint256 _index) external {
-        require(isSeller(msg.sender), "Cannot abort: No sales found");
+        require(isSeller(msg.sender), "Gotchiswap: No sales found for the seller");
+        // Get the Aavegotchi ID for the sale to be aborted
         uint256 gotchi = sellers[msg.sender][_index].gotchi;
+        // Remove the sale from the seller's sales list
         removeSale(msg.sender, _index);
+        // Transfer the Aavegotchi token back to the seller
         aavegotchi.safeTransferFrom(address(this), msg.sender, gotchi, "");
         emit abortSale(msg.sender, gotchi);
     }
 
+    /**
+     * @dev Allows a buyer to purchase an Aavegotchi from his offers.
+     * @param _index The index of the offer to be accepted.
+     */
     function buyGotchi(uint256 _index) external {
-        require(isBuyer(msg.sender), "Cannot buy: No offers found");
+        require(isBuyer(msg.sender), "Gotchiswap: No offers found for the buyer");
 
+        // Get the details of the offer to be accepted
         address seller = buyers[msg.sender][_index].seller;
         uint256 id = buyers[msg.sender][_index].id;
         uint256 sale_index = getSaleIndex(seller, id);
@@ -153,17 +233,21 @@ contract Gotchiswap is Initializable {
         uint256 gotchi = sale.gotchi;
         uint256 price = sale.price;
 
+        // Remove the offer from the buyer's offers list
         removeSale(seller, sale_index);
 
-        // deposit amount to contract
+        // Deposit the GHST amount to the contract
         SafeERC20.safeTransferFrom(GHST, msg.sender, address(this), price);
-        // transfer gotchi to buyer
+        // Transfer the Aavegotchi token to the buyer
         aavegotchi.safeTransferFrom(address(this), msg.sender, gotchi, "");
-        // send amount to seller
+        // Send the GHST amount to the seller
         SafeERC20.safeTransfer(GHST, seller, price);
         emit concludeSale(msg.sender, gotchi);
     }
 
+    /**
+     * @dev The ERC721 receiver callback function (for compatibility).
+     */
     function onERC721Received(
         address, /* _operator */
         address, /*  _from */
@@ -179,78 +263,108 @@ contract Gotchiswap is Initializable {
         );
     }
 
+    /**
+     * @dev Private function to get a unique ID for each sale.
+     * @return A unique sale ID.
+     */
     function getSaleId() private returns (uint) {
-      return saleId++;
+        return saleId++;
     }
 
+    /**
+     * @dev Private function to get the index of a sale in the seller's sales list.
+     * @param _seller The address of the seller.
+     * @param _id The ID of the sale.
+     * @return index The index of the sale in the seller's sales list.
+     */
     function getSaleIndex(address _seller, uint256 _id) private view returns (uint256 index) {
         for (uint i = 0; i < sellers[_seller].length; i++) {
-	        if (sellers[_seller][i].id == _id) {
+            if (sellers[_seller][i].id == _id) {
                 return i;
             }
         }
-        revert("Cannot get index: Sale not found");
+        revert("Gotchiswap: Sale not found");
     }
 
+    /**
+     * @dev Private function to add a sale to the seller's sales list and the buyer's offers list.
+     * @param _seller The address of the seller.
+     * @param _gotchi The ID of the Aavegotchi token to be sold.
+     * @param _price The price of the Aavegotchi token in GHST tokens.
+     * @param _buyer The address of the buyer who can purchase the Aavegotchi token.
+     */
     function addSale(
         address _seller,
         uint256 _gotchi,
         uint256 _price,
         address _buyer
-    ) private
-    {
-        // add sale to seller
+    ) private {
+        // Add the sale to the seller's sales list
         uint256 id = getSaleId();
         sellers[_seller].push(GotchiSale(id, _gotchi, _price, _buyer));
-        // add reference to buyer
+        // Add a reference to the sale in the buyer's offers list
         buyers[_buyer].push(SaleRef(_seller, id));
     }
 
+    /**
+     * @dev Private function to check if an address has any active sales.
+     * @param _seller The address to check.
+     * @return True if the address has active sales, otherwise false.
+     */
     function isSeller(address _seller) private view returns (bool) {
         if (sellers[_seller].length > 0) {
-          return true;
+            return true;
         }
         return false;
     }
 
+    /**
+     * @dev Private function to remove a sale from the seller's sales list and the buyer's offers list.
+     * @param _seller The address of the seller.
+     * @param _index The index of the sale to be removed.
+     */
     function removeSale(address _seller, uint256 _index ) private {
-        require(isSeller(_seller), "Cannot remove sale: No sales found");
+        require(isSeller(_seller), "Gotchiswap: No sales found for the seller");
 
-        // the buyer for that sale
+        // Get the buyer and Aavegotchi ID for the sale to be removed
         address buyer = sellers[_seller][_index].buyer;
-        // the gotchi for that sale
         uint256 id = sellers[_seller][_index].id;
-        // number of sales for that buyer
+        // Get the number of offers for that buyer
         uint256 buyer_sales = buyers[buyer].length;
 
-        // for each sales of the buyer
-        for (uint i=0; i < buyer_sales; i++) {
-            // if from that seller
+        // Loop through each sale made by the buyer
+        for (uint i = 0; i < buyer_sales; i++) {
+            // Check if the sale is from that seller
             if (buyers[buyer][i].seller == _seller) {
-                // check if it is for that sale
+                // Check if the sale ID matches the one to be removed
                 if (buyers[buyer][i].id == id) {
-                    // if so remove offer
-                    for (uint j=i; j < buyer_sales -1; j++) {
-                        buyers[buyer][j] = buyers[buyer][j+1];
+                    // If so, remove the offer from the stack
+                    for (uint j = i; j < buyer_sales - 1; j++) {
+                        buyers[buyer][j] = buyers[buyer][j + 1];
                     }
-                    // remove last offer
+                    // And remove the duplicate on top
                     buyers[buyer].pop();
                     break;
                 }
             }
         }
 
-        // update seller's sales
+        // Update the seller's list of sales
         uint256 length = sellers[_seller].length;
-        // remove sale (preserve order)
+        // Remove the sale (preserve order)
         for (uint i = _index; i < length - 1; i++) {
-	        sellers[_seller][i] = sellers[_seller][i+1];
+            sellers[_seller][i] = sellers[_seller][i + 1];
         }
         sellers[_seller].pop();
     }
 
+    /**
+     * @dev Private function to check if an address has any active offers.
+     * @param _buyer The address to check.
+     * @return True if the address has active offers, otherwise false.
+     */
     function isBuyer(address _buyer) private view returns (bool) {
-        // default is false
+        // Default is false
         if (buyers[_buyer].length > 0) {
             return true;
         }
