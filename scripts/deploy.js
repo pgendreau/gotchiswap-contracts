8bdb5a7257d8ff09a42539a7f1ed58a38f5d63ef
Philippe Gendreau <pgendreau@gmail.com>
2023-08-15 23:09:14 -0400

    deploy to polygon

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,8 +3,9 @@ const hre = require("hardhat");
 async function main() {
 
   // mumbai
-  const AdminAddress = "0x5DaFd030C07844741157CcDcc366306822dd5FF3";
-  //const AdminAddress = "0x43FF4C088df0A425d1a519D3030A1a3DFff05CfD";
+  //const AdminAddress = "0x5DaFd030C07844741157CcDcc366306822dd5FF3";
+  // polygon
+  const AdminAddress = "0x43FF4C088df0A425d1a519D3030A1a3DFff05CfD";
 
   const Gotchiswap = await hre.ethers.getContractFactory("Gotchiswap");
   const gotchiswap = await hre.upgrades.deployProxy(
