7956c56cfd12882cc92e8631089771fc5b5694b5
Philippe Gendreau <pgendreau@gmail.com>
2023-08-20 22:08:52 -0400

    deploy non-reentrant version

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,10 +7,13 @@ async function main() {
   // polygon
   const AdminAddress = "0x43FF4C088df0A425d1a519D3030A1a3DFff05CfD";
 
+  const feeData = await hre.ethers.provider.getFeeData();
+
   const Gotchiswap = await hre.ethers.getContractFactory("Gotchiswap");
   const gotchiswap = await hre.upgrades.deployProxy(
     Gotchiswap,
-    [AdminAddress]
+    [AdminAddress],
+    feeData
   );
 
   await gotchiswap.waitForDeployment();
@@ -18,7 +21,9 @@ async function main() {
   console.log("Gotchiswap proxy deployed to:", await gotchiswap.target);
 }
 
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
 });
--- scripts/upgrade.js
+++ scripts/upgrade.js
@@ -2,7 +2,7 @@ const { ethers, upgrades } = require("hardhat");
 
 async function main() {
   const Gotchiswap = await ethers.getContractFactory("Gotchiswap");
-  const gotchiswap = await upgrades.upgradeProxy("0x022a644b60a63BC6Dd51bd7Eab67E73F4f5a14d2", Gotchiswap);
+  const gotchiswap = await upgrades.upgradeProxy("0xFE4B96f1860c5A2A09CD4bD5C341632c9E9486e6", Gotchiswap);
   console.log("Gotchiswap upgraded");
 }
 
